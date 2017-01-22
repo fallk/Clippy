2f0d25ad90d8dac8337ceaca3b1b8c38f2663b36
chrishansen69 <cadrekucra@gmx.com>
2017-01-22 15:08:46 -0200

    simplify + change default output format to plain text, not HTML

--- Clippy/Program.cs
+++ Clippy/Program.cs
@@ -10,51 +10,49 @@ namespace Clippy
         {
             if (args.Length > 0)
             {
-                foreach (var s in args)
+                var s = args[0];
+                var t = TextDataFormat.Text;
+                switch (s)
                 {
-                    var t = TextDataFormat.Text;
-                    switch (s)
-                    {
-                        case "html":
-                            t = TextDataFormat.Html;
-                            break;
-                        case "rtf":
-                            t = TextDataFormat.Rtf;
-                            break;
-                        case "csv":
-                            t = TextDataFormat.CommaSeparatedValue;
-                            break;
-                        case "-h":
-                        case "--help":
-                        case "-help":
-                        case "/?":
-                            Console.WriteLine("Clippy");
-                            Console.WriteLine();
-                            Console.WriteLine("  Usage: ");
-                            Console.WriteLine("  - clippy [html|rtf|csv|<empty>]: Get clipboard text in a format. If not specified, the format is plain text.");
-                            Console.WriteLine("  - clippy -h: Show this help message.");
-                            Environment.Exit(0);
-                            break;
-                        default:
-                            t = TextDataFormat.Text;
-                            break;
-                    }
-                    if (Clipboard.ContainsText(t))
-                    {
-                        Console.Write(Clipboard.GetText(t));
-                    }
-                    else
-                    {
-                        // print any clipboard content
-                        Console.Write(Clipboard.GetText());
-                        // exit with error
-                        Environment.Exit(1);
-                    }
+                    case "html":
+                        t = TextDataFormat.Html;
+                        break;
+                    case "rtf":
+                        t = TextDataFormat.Rtf;
+                        break;
+                    case "csv":
+                        t = TextDataFormat.CommaSeparatedValue;
+                        break;
+                    case "-h":
+                    case "--help":
+                    case "-help":
+                    case "/?":
+                        Console.WriteLine("Clippy");
+                        Console.WriteLine();
+                        Console.WriteLine("  Usage: ");
+                        Console.WriteLine("  - clippy [html|rtf|csv|<empty>]: Get clipboard text in a format. If not specified, the format is plain text.");
+                        Console.WriteLine("  - clippy -h: Show this help message.");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        t = TextDataFormat.Text;
+                        break;
+                }
+                if (Clipboard.ContainsText(t))
+                {
+                    Console.Write(Clipboard.GetText(t));
+                }
+                else
+                {
+                    // print any clipboard content
+                    Console.Write(Clipboard.GetText());
+                    // exit with error
+                    Environment.Exit(1);
                 }
             }
             else
             {
-                Console.Write(Clipboard.GetText(TextDataFormat.Html));
+                Console.Write(Clipboard.GetText(TextDataFormat.Text));
             }
         }
     }
